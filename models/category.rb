97c40f969e7bbea6a4d8d468015f3056a9ab4ce1
Abigaila Ekierte <arekiert@gmail.com>
2019-02-08 15:14:46 +0000

    adding methods to category class

--- models/category.rb
+++ models/category.rb
@@ -10,5 +10,44 @@ attr_reader :id
     @name = type['name']
   end
 
+  def save()
+    sql = "INSERT INTO categories
+          (name) VALUES ($1)
+          RETURNING id"
+    values = [@name]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+  end
+
+  def update()
+    sql = "SELECT * FROM categories WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM categories WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql,values)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM categories
+          WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run( sql, values )
+    return Category.new( results.first )
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM categories"
+    SqlRunner.run(sql)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM categories"
+    result = SqlRunner.run(sql).first
+    return result.map{ |type| Category.new(type)}
+  end
 
 end
